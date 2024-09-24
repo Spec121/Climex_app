3e3b818f95b81908e8e31c2f8ee9652ff01fd231
PC <wkiu2004@gmail.com>
2024-09-24 05:32:34 -0300

    sa

--- app/src/main/java/com/example/climex_app/MainActivity3.java
+++ app/src/main/java/com/example/climex_app/MainActivity3.java
@@ -1,13 +1,32 @@
 package com.example.climex_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    Button buttonvuelta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3); // Asegúrate de que este layout exista
+        setContentView(R.layout.activity_main3);
+
+        buttonvuelta = (Button) findViewById(R.id.buttonvuelta);
+
+        // Configuramos el click listener para el botón
+        buttonvuelta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Creamos un Intent para volver a MainActivity2
+                Intent intent = new Intent(MainActivity3.this, MainActivity2.class);
+                startActivity(intent); // Iniciamos MainActivity2
+                finish(); // Finalizamos MainActivity3 para que no quede en el historial
+            }
+        });
     }
 }
